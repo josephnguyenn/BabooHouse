a5f91d5c2f8d4a52d3518c6d2dd82007fb555dd3
Tien Huynh <mineshark15@gmail.com>
2025-02-13 16:55:12 +0700

    fix pending

--- templates/pending_buildings.php
+++ templates/pending_buildings.php
@@ -4,13 +4,9 @@ include '../admin/getallbuilding.php';
 include '../admin/getallroom.php';
 include '../admin/getalluser.php';
 
-$min_price = isset($_GET['min_price']) ? (int)$_GET['min_price'] : getMaxAndMinPrice()['min'];
-$max_price = isset($_GET['max_price']) ? (int)$_GET['max_price'] : getMaxAndMinPrice()['max'];
-$selected_types = isset($_GET['building_type']) ? $_GET['building_type'] : NULL;
-$status_type = isset($_GET['status_type']) ? $_GET['status_type'] : NULL;
-$city = isset($_GET['city']) ? $_GET['city'] : NULL;
-$district = isset($_GET['district']) ? $_GET['district'] : NULL;
-$buildings = getAllBuildings($min_price, $max_price, $selected_types, $_SESSION['user_id'], $status_type, $city, $district);
+$min_price = getMaxAndMinPrice()['min'];
+$max_price = getMaxAndMinPrice()['max'];
+$buildings = getAllBuildings();
 $building_types = getDistinctBuildingTypes();
 ?>
 
@@ -20,21 +16,16 @@ $building_types = getDistinctBuildingTypes();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thông tin lưu trú</title>
+    <title>Toà nhà chờ duyệt</title>
     <link href="../assets/css/filter.css" rel="stylesheet"> 
 </head>
 <body>
     <?php include '../includes/header.php'; ?>
     <div class="head-container">
-        <?php include '../includes/filter_accommodation.php'; ?>
         <div class="main-content">
             <div class="manage-head">
-                <h1>Thông Tin Lưu Trú</h1>
+                <h1>Toà nhà chờ duyệt</h1>
             </div>
-            <div class="icon-container">
-                <a id="filter-icon" aria-haspopup="true" aria-expanded="false" onclick="toggleFilter()"><img src="../assets/icons/filter.svg"></a>
-            </div>
-            <?php include '../includes/filter_exe_address.php' ?>
         <div style="overflow-x: auto; width: 100%;">
         <table>
                 <tr>
@@ -91,6 +82,5 @@ $building_types = getDistinctBuildingTypes();
         </div>
         <?php include '../includes/sidebar.php'; ?>
     </div>
-    <script src="../assets/js/filter.js"></script>
 </body>
 </html>
